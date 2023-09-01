662b076de7d9f0992b823de0ddb7e77f300e032d
joedupor <joedupor@gmail.com>
2023-09-01 00:38:57 -0500

    init commit set 01

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,7 +6,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
+            if (num1 == num2)
             {
                 return true;
             }
@@ -16,34 +16,49 @@ namespace ChallengesWithTestsMark8
             }
         }
 
-        public double Subtract(double minuend, double subtrahend)
+        public double subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+           return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else 
+            {
+                return number2;
+            } 
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
-        }
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
 
+            else
+            {
+                return $"Hello, {nameOfPerson}!";  //("John", "Hello, John!")
+            }
+        }
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
