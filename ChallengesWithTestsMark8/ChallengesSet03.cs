9bd1cfa27a6a43e0c684c379aebcb918c678c46f
joedupor <joedupor@gmail.com>
2023-09-24 18:15:26 -0500

    completed set 3

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,53 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet03
     {
-        public bool ArrayContainsAFalse(bool[] vals)
+        public bool ArrayContainsAFalse(bool[] vals)     //1.
         {
-            throw new NotImplementedException();
+            foreach (bool item in vals)
+            {
+                if (item == false)
+                    return true;
+            }
+            return false;
         }
 
-        public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
+        public bool IsSumOfOddsOdd(IEnumerable<int> numbers)     //2.
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+
+            int sumOfOdds = numbers.Sum();
+
+            if (sumOfOdds % 2 != 0)
+            {
+                return true;
+            }
+
+            return false;
         }
 
-        public bool PasswordContainsUpperLowerAndNumber(string password)
+        public bool PasswordContainsUpperLowerAndNumber(string password)     //3.
         {
-            throw new NotImplementedException();
+            bool isLower = false;
+            bool isUpper = false;
+            bool isNumber = false;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLower(password[i]))
+                {
+                    isLower = true;
+                }
+
+                if (char.IsUpper(password[i]))
+                {
+                    isUpper = true;
+                }
+
+                if (char.IsNumber(password[i]))
+                {
+                    isNumber = true;
+                }
+            }
+
+            if (isLower == true && isUpper == true && isNumber == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        public char GetFirstLetterOfString(string val)
+        public char GetFirstLetterOfString(string val)       //4.
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
-        public char GetLastLetterOfString(string val)
+        public char GetLastLetterOfString(string val)    //5.
         {
-            throw new NotImplementedException();
+            return val[val.Length - 1];
         }
 
-        public decimal Divide(decimal dividend, decimal divisor)
+        public decimal Divide(decimal dividend, decimal divisor)     //6.
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+            }
+            
+            return dividend / divisor;
         }
 
-        public int LastMinusFirst(int[] nums)
+        public int LastMinusFirst(int[] nums)      //7.
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
-        public int[] GetOddsBelow100()
+        public int[] GetOddsBelow100()       //8.
         {
-            throw new NotImplementedException();
+            List<int> oddsBelow100 = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+                if (i % 2 != 0)
+                {
+                    oddsBelow100.Add(i);
+                }
+            return oddsBelow100.ToArray();
         }
 
-        public void ChangeAllElementsToUppercase(string[] words)
+        public void ChangeAllElementsToUppercase(string[] words)     //9.
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
+            
+            
         }
     }
 }
