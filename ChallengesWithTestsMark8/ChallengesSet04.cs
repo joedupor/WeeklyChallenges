5db426982f3318af70fc4326cacb8833ca65407e
joedupor <joedupor@gmail.com>
2023-10-09 09:40:43 -0500

    completed set 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,52 +1,143 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet04
     {
-        public int AddEvenSubtractOdd(int[] numbers)
+        public int AddEvenSubtractOdd(int[] numbers)                             //1.
         {
-            throw new NotImplementedException();
+            {
+                int total = 0;
+                foreach (int number in numbers)
+                {
+                    if (number % 2 == 0)
+                    {
+                        total += number;
+                    }
+
+                    if (number % 2 != 0)
+                    {
+                        total -= number;
+                    }
+                }
+
+                return total;
+
+
+            }
         }
 
-        public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
+        public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)    //2.
         {
-            throw new NotImplementedException();
+            var list = new List<int> { str1.Length, str2.Length, str3.Length, str4.Length };
+
+            return list.Min();
         }
 
-        public int GetSmallestNumber(int number1, int number2, int number3, int number4)
+        public int GetSmallestNumber(int number1, int number2, int number3, int number4)        //3.
         {
-            throw new NotImplementedException();
+            var list = new List<int>() { number1, number2, number3, number4 };
+
+            return list.Min();
         }
 
-        public void ChangeBusinessNameTo_TrueCoders(Business biz)
+        public void ChangeBusinessNameTo_TrueCoders(Business biz)           //4.
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
-        public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
+        public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)        //5.
         {
-            throw new NotImplementedException();
+            if (sideLength1 + sideLength2 > sideLength3 &&
+                    sideLength1 + sideLength3 > sideLength2 &&
+                    sideLength2 + sideLength3 > sideLength1) 
+            {
+                return true;
+            }
+
+            return false;
         }
 
-        public bool IsStringANumber(string input)
+        public bool IsStringANumber(string input)                           //6.
         {
-            throw new NotImplementedException();
+            bool isANumber = double.TryParse(input, out double number);
+
+            return isANumber;
         }
 
-        public bool MajorityOfElementsInArrayAreNull(object[] objs)
-        {
-            throw new NotImplementedException();
-        }
+        public bool MajorityOfElementsInArrayAreNull(object[] objs)         //7.
+            {
+                var amountOfElements = objs.Length;
+                var majorityOfEs = (amountOfElements / 2) + 1;
 
-        public double AverageEvens(int[] numbers)
-        {
-            throw new NotImplementedException();
-        }
+                var count = 0;
 
-        public int Factorial(int number)
+                foreach (string item in objs)
+                {
+                    if (item == null)
+                    {
+                        count++;
+                    }
+                }
+
+                if (count >= majorityOfEs)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+
+        public double AverageEvens(int[] numbers)                           //8.
+            {
+                double sumTotal = 0;
+                double numOfElements = 0;
+
+                if (numbers == null)
+                {
+                    return 0;
+                }
+
+                foreach (int item in numbers)
+                {
+                    if (item % 2 == 0)
+                    {
+                        sumTotal += item; 
+                        numOfElements++;
+                    }
+                }
+
+                if (numOfElements > 0)
+                {
+                    return sumTotal / numOfElements;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+        public int Factorial(int number)                                    //9.
         {
-            throw new NotImplementedException();
+            int factoredNumber = 1;
+
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            for (int i = number; i > 0; i--)
+            {
+                factoredNumber *= i;
+            }
+
+            return factoredNumber;
         }
     }
 }
